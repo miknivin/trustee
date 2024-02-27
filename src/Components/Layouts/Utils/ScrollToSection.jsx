16c52ebbfec90487c361d0a8c295d7a6cabd1d7f
miknivin <116654570+miknivin@users.noreply.github.com>
2024-02-27 19:18:58 +0530

    Update ScrollToSection.jsx

--- src/Components/Layouts/Utils/ScrollToSection.jsx
+++ src/Components/Layouts/Utils/ScrollToSection.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { animateScroll as scroll } from "react-scroll";
 
 const ScrollToSection = () => {
   const location = useLocation();
@@ -9,12 +8,12 @@ const ScrollToSection = () => {
     const queryParams = new URLSearchParams(location.search);
     const sectionId = queryParams.get("section");
     if (sectionId) {
-      scroll.scrollTo(`#${sectionId}`, {
-        duration: 800, // adjust duration as needed
-        smooth: "easeInOutQuart" // adjust smoothness as needed
-      });
+      const element = document.getElementById(sectionId);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
     }
-  }, [location.search]);
+  }, []);
 
   return null;
 };
